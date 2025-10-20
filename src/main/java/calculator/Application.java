0b6a2cd4a66c8776b3500a1c4f8ea0d9270ae477
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-20 19:15:28 +0900

    feat: 기본 구분자 이용 SUM 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public int calculateSum(String input) {
+        int sum = 0;
+        String str = input;
+
+        if (str.isEmpty()) {
+            return 0;
+        }
+        
+        if (Character.isDigit(str.charAt(0))) {
+            String[] numbers = str.split("[,:]");
+            for (String number : numbers) {
+                int num = Integer.parseInt(number);
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
+
+   
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = app.calculateSum(input);
+        System.out.println("결과 : "+ result);
     }
 }
