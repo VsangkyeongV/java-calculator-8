d716749b9789329509f2302b07cb7fde5d941137
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-20 19:59:25 +0900

    feat: exception 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -18,23 +18,17 @@ public class Application {
                 int num = Integer.parseInt(number);
                 sum += num;
             }
-        }
-        
-        if (str.startsWith("//")) {
-            System.out.println(str.indexOf("\\n"));
-            
+        } else if (str.startsWith("//")) {
             String customString = str.substring(2, str.indexOf("\\n"));
-            
-            System.out.println("customString: " + customString);
-            
             String numString = str.substring(str.indexOf("\\n") + 2);
-            System.out.println("numString: " + numString);
             String[] numbers = numString.split("["+customString+"]");
             
             for (String number : numbers) {
                 int num = Integer.parseInt(number);
                 sum += num;
             }
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
         }
 
         return sum;
