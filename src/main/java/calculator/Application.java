6639d047509e21ff08a249655ce3bae219b31b14
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-20 20:21:20 +0900

    refactor: 코드 리팩토링
    1. 중복된 부분 함수 하나로 묶기
    2. main함수 간결화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,42 +4,49 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
-    public int calculateSum(String input) {
+    public int Sum(String[] numbers) {
         int sum = 0;
-        String str = input;
+        for (String number : numbers) {
+                int num = Integer.parseInt(number);
+                sum += num;
+            }
+        return sum;
+    }
+
+    public int calculateSum(String str) {
+        int sum = 0;
+        String[] numbers;
 
         if (str.isEmpty()) {
-            return 0;
+
         }
-        
-        if (Character.isDigit(str.charAt(0))) {
-            String[] numbers = str.split("[,:]");
-            for (String number : numbers) {
-                int num = Integer.parseInt(number);
-                sum += num;
-            }
-        } else if (str.startsWith("//")) {
+
+        if (Character.isDigit(str.charAt(0))) {//기본 구분자
+            numbers = str.split("[,:]");
+
+        } else if (str.startsWith("//")) {//커스텀 구분자
             String customString = str.substring(2, str.indexOf("\\n"));
             String numString = str.substring(str.indexOf("\\n") + 2);
-            String[] numbers = numString.split("["+customString+"]");
-            
-            for (String number : numbers) {
-                int num = Integer.parseInt(number);
-                sum += num;
-            }
+            numbers = numString.split("[" + customString + "]");
+
         } else {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
 
+        sum = Sum(numbers);
+
         return sum;
     }
 
-   
-    public static void main(String[] args) {
-        Application app = new Application();
+    public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        int result = app.calculateSum(input);
+        int result = calculateSum(input);
         System.out.println("결과 : "+ result);
+    }
+   
+    public static void main(String[] args) {
+        new Application().run();
+       
     }
 }
